d26ed1c8db36b64683d9d9dad2d0b3d2b9caaaf9
4nh3k <21520579@gm.uit.edu.vn>
2023-11-09 15:41:23 +0700

    update tailwind config

--- Template1/src/pages/Login/Login.tsx
+++ Template1/src/pages/Login/Login.tsx
@@ -1,3 +1,11 @@
 export default function Login() {
-  return <div></div>;
+  return (
+    <div className='w-screen h-screen bg-ladingBg bg-no-repeat bg-right flex'>
+      <img
+        className='w-[39.4rem] h-[29.18rem]'
+        src='/src/assets/img/illustration.svg'
+        alt='reading book'
+      ></img>
+    </div>
+  );
 }
--- Template1/tailwind.config.js
+++ Template1/tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif']
+      },
+      backgroundImage: {
+        readingBook: "url('/src/assets/img/illustration.svg')",
+        ladingBg: "url('/src/assets/img/bg.svg')",
+      },
+      colors: {
+        background: '#F2F6FF',
+        primary: '#8352FD',
+        primaryBorder: '#C0BBF4',
+        primary2: '#3913B8',
+        neutralDark: '#1A2151',
+        neutralLight: '#E3E8FF',
+        secondary1: '#00CFFD',
+        secondary2: '#2FB5FC',
+        textboxBorder: '#E0E0E0'
+      },
+      borderWidth: {
+        1: '1px',
+        6: '6px'
+      },
+      outlineWidth: {
+        6: '6px'
+      }
+    }
   },
   plugins: []
 };
