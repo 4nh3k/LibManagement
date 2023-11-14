4c90c5d015600f00839313300ad25832129abc25
4nh3k <21520579@gm.uit.edu.vn>
2023-11-14 11:22:40 +0700

    Update table to render React components

--- Template1/src/pages/ReturnCard/ReturnCardList.tsx
+++ Template1/src/pages/ReturnCard/ReturnCardList.tsx
@@ -5,12 +5,20 @@ const ReturnCardList = () => {
   const data = [
     { returnCardId: 1, adminId: 1, action: 'Return' },
     { returnCardId: 2, adminId: 2, action: 'Return' },
-    { returnCardId: 3, adminId: 3, action: 'Return' }
+    {
+      returnCardId: 3,
+      adminId: 3,
+      action: (
+        <button type='button' className='primary-btn'>
+          auto
+        </button>
+      )
+    }
   ];
 
   return (
     <div>
-      <h2>Borrow Card List</h2>
+      <h2 className='font-bold text-xl mb-4'>Borrow Card List</h2>
       <Table
         headers={[
           { title: 'Return Card ID', dataIndex: 'returnCardId' },
--- Template1/src/pages/Transactions/Transactions.tsx
+++ Template1/src/pages/Transactions/Transactions.tsx
@@ -1,14 +1,6 @@
-import React from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import ReturnCard from '../ReturnCard/ReturnCard';
-import Table from 'src/components/Table/Table';
 import ReturnCardList from '../ReturnCard/ReturnCardList';
-const headers = ['Name', 'Age', 'Email'];
-const data = [
-  { Name: 'John', Age: 25, Email: 'john@example.com' },
-  { Name: 'Jane', Age: 30, Email: 'jane@example.com' },
-  { Name: 'Bob', Age: 40, Email: 'bob@example.com' }
-];
 
 const Transactions = () => {
   return (
@@ -24,9 +16,7 @@ const Transactions = () => {
         <TabPanel>
           <ReturnCard />
         </TabPanel>
-        <TabPanel>
-          <Table headers={headers} data={data} />
-        </TabPanel>
+        <TabPanel></TabPanel>
         <TabPanel>
           <ReturnCardList></ReturnCardList>
         </TabPanel>
