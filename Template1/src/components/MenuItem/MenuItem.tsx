6ff2343ccafd0213d35de4b747f91f3dc1a56667
An Hung <anbeel191@gmail.com>
2023-12-18 22:19:54 +0700

    Coi ai add lẹ hơn

--- Template1/src/components/MenuItem/MenuItem.tsx
+++ Template1/src/components/MenuItem/MenuItem.tsx
@@ -10,16 +10,15 @@ interface MenuItemProps {
 
 export function MenuItem({ iconSrc, label, to }: MenuItemProps) {
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
     <NavLink
-      to={`/${to || label.toLowerCase()}`}
+      to={`/${to === '' || to ? to : label.toLowerCase()}`}
       className={({ isActive }) =>
         `hover:bg-gray-300 lg:w-36 w-fit px-4 py-2 mx-0 lg:px-2 menu-item flex items-center gap-2 ${
           isActive ? 'bg-gray-300' : ''
         }`
       }
     >
-      <img alt='menu-icon' src={iconSrc} className='w-8 h-8 md:w-8 md:h-8'></img>
+      <img alt='menu-icon' src={iconSrc} className='w-8 h-8 md:w-8 md:h-8' />
       <span className='hidden lg:block font-medium text-sm'>{label}</span>
     </NavLink>
   );
--- Template1/src/components/Sidebar/Sidebar.tsx
+++ Template1/src/components/Sidebar/Sidebar.tsx
@@ -4,20 +4,21 @@ import book_img from '../../assets/img/book.png';
 import member_img from '../../assets/img/group-chat.png';
 import transaction_img from '../../assets/img/transaction.png';
 import MenuItem from 'src/components/MenuItem';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div className='bg-sidebarColor shadow fixed top-0 left-0 bottom-0'>
       <div className='flex flex-col items-center justify-center gap-4'>
-        <div className='text-center items-center justify-center mx-1 lg:mx-2'>
+        <Link to='/' className='text-center items-center justify-center mx-1 lg:mx-2'>
           <img
             alt='Website Logo'
             src={website_logo}
             className='p-2 text-center w-[3.75rem] h-[3.75rem]'
           />
           <span className='font-bold text-xl hidden lg:inline-block'>LibMa</span>
-        </div>
-        <MenuItem iconSrc={library_img} label='Library' />
+        </Link>
+        <MenuItem iconSrc={library_img} label='Library' to='' />
         <MenuItem iconSrc={book_img} label='Book' to='books' />
         <MenuItem iconSrc={member_img} label='Member' />
         <MenuItem iconSrc={transaction_img} label='Transaction' to='transactions' />
