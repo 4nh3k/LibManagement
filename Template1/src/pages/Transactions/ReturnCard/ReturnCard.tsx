0275e068ca993166c1a87e3cfe47c2ef93cf9ec3
4nh3k <21520579@gm.uit.edu.vn>
2023-12-19 15:05:20 +0700

    revert

--- Template1/src/pages/Transactions/ReturnCard/ReturnCard.tsx
+++ Template1/src/pages/Transactions/ReturnCard/ReturnCard.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from '@tanstack/react-query';
-import { returnCardApi } from 'src/apis/return-form.api';
 import Table from 'src/components/Table/Table';
 
 interface ReturnCardProps {
@@ -9,39 +7,30 @@ interface ReturnCardProps {
 const ReturnCard: React.FC<ReturnCardProps> = ({ onToggle }) => {
   const headers = [
     { title: 'Return Card ID', dataIndex: 'returnCardId' },
-    { title: 'Username', dataIndex: 'username' },
-    { title: 'Return Date', dataIndex: 'returnDate' },
-    { title: 'Fee', dataIndex: 'fee' }
+    { title: 'Admin ID', dataIndex: 'adminId' },
+    { title: 'Action', dataIndex: 'action' }
   ];
 
-  const { data: ReturnCardData, isLoading } = useQuery({
-    queryKey: ['ReturnCard'],
-    queryFn: () => returnCardApi.getAllReturnCard(),
-    select: data => {
-      return data.data.data.doc.map((item: ReturnCard) => {
-        return {
-          returnCardId: item._id,
-          username: item.borrower.fullName,
-          returnDate: new Date(item.returnDate).toLocaleDateString('en-GB'),
-          fee: item.fee.toFixed(2),
-          action: (
-            <button type='button' className='primary-btn'>
-              auto
-            </button>
-          )
-        };
-      });
+  const data = [
+    { returnCardId: 1, adminId: 1, action: 'Return' },
+    { returnCardId: 2, adminId: 2, action: 'Return' },
+    { returnCardId: 2, adminId: 2, action: 'Return' },
+    {
+      returnCardId: 3,
+      adminId: 3,
+      action: (
+        <button type='button' className='primary-btn'>
+          auto
+        </button>
+      )
     }
-  });
-
-  if (isLoading) return <div>Loading...</div>;
-  console.log(ReturnCardData);
+  ];
 
   return (
     <div>
       <div id='body' className='mt-5 m-3 lg:mr-20'>
         <span className='text-xl font-bold'>Return Card List</span>
-        <Table headers={headers} data={ReturnCardData} onToggle={onToggle}></Table>
+        <Table headers={headers} data={data} onToggle={onToggle}></Table>
       </div>
     </div>
   );
