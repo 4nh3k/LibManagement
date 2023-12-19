e4fe8ebb0291225aee5c5c3a29ed2b3da0c97be4
An Hung <anbeel191@gmail.com>
2023-12-19 09:58:33 +0700

    ADd comment

--- Template1/src/layouts/MainLayout/MainLayout.tsx
+++ Template1/src/layouts/MainLayout/MainLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from 'src/components/Sidebar';
 export default function MainLayout() {
   return (
-    <div className='h-screen w-screen'>
+    <div className='h-screen w-screen overflow-y-hidden'>
       <Sidebar />
-      <div className='bg-background w-[calc(100%-4rem)] lg:w-[calc(100%-9rem)] h-screen ml-auto'>
+      <div className='bg-white w-[calc(100%-4.2rem)] lg:w-[calc(100%-9rem)] h-screen ml-auto overflow-y-auto'>
         <Outlet />
       </div>
     </div>
