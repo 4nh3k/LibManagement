806a7bd54692be6d6c44f1c405141d8ced4c8e3f
4nh3k <21520579@gm.uit.edu.vn>
2024-01-01 09:53:34 +0700

    Remove unused import and update RatingStar props

--- Template1/src/components/BookCard/BookCard.tsx
+++ Template1/src/components/BookCard/BookCard.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import RatingStar from '../RatingStar/RatingStar';
 
 interface BookProps {
@@ -15,7 +15,7 @@ function BookCard({ coverImg, title, overview, rating, id }: BookProps) {
       <img className='w-32 h-48' src={coverImg} alt={title} crossOrigin='anonymous' />
       <div className='w-32 ml-2 flex flex-col justify-between flex-grow'>
         <h2 className='text-primary2 text-lg font-bold line-clamp-3'>{title}</h2>
-        <RatingStar rating={rating} />
+        <RatingStar initialValue={rating} readonly />
         <p className='text-gray-500 h-12 text-xs font-semibold line-clamp-3'>{overview}</p>
         <button className='bg-gray-500 hover:opacity-80 text-white w-24 text-sm font-medium h-8 rounded-md'>
           See more
